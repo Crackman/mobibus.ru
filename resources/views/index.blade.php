14c9b39fe7955b9db4978026d39b54016421bfff
Baldis <bitochek67@mail.ru>
2022-04-21 13:00:52 +0300

    Update content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,14 @@
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-	<link href="../../public/images/favicon.ico" rel="icon" type="image/x-icon" />
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+	<link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
 </head>
 <body>
 <div class="success">Ваша заявка отправлена!</div>
@@ -257,6 +264,190 @@
 
 		</div>
 	</div>
+
+	<div class="container">
+
+		<table class="table table-sm table-hover">
+			<thead>
+			<tr>
+				<th colspan="3" class="text-center" scope="col">Выезд и диагностика</th>
+			</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Выезд специалиста</td>
+					<td>300</td>
+				</tr>
+				<tr>
+					<td>Диагностика</td>
+					<td><strong style="color: dodgerblue">Бесплатно</strong></td>
+			</tr>
+
+				<tr>
+					<th colspan="3" class="text-center" scope="col">Системный блок</th>
+				</tr>
+
+				<tr>
+					<td>Чистка от пыли (сжатым воздухом)</td>
+					<td>350</td>
+				</tr>
+				<tr>
+					<td>Чистка от пыли (c разбором блока питания)</td>
+					<td>500</td>
+				</tr>
+				<tr>
+					<td>Полная сборка системного блока</td>
+					<td>700</td>
+				</tr>
+
+				<tr>
+					<td>Установка / замена блока питания</td>
+					<td>250</td>
+				</tr>
+				<tr>
+					<td>Установка HDD/SDD</td>
+					<td>200</td>
+				</tr>
+				<tr>
+					<td>Установка HDD/SDD</td>
+					<td>200</td>
+				</tr>
+				<tr>
+					<td>Установка оперативной памяти</td>
+					<td>200</td>
+				</tr>
+				<tr>
+					<td>Установка процессора</td>
+					<td>500</td>
+				</tr>
+				<tr>
+					<td>Установка видео / звуковой / сетевой платы/ TV-тюнера</td>
+					<td>250</td>
+				</tr>
+				<tr>
+					<td>Замена корпуса, переборка компьютера</td>
+					<td>700</td>
+				</tr>
+				<tr>
+					<td>Ремонт простой (без пайки)</td>
+					<td>300</td>
+				</tr>
+				<tr>
+					<td>Ремонт с пайкой</td>
+					<td>от 500</td>
+				</tr>
+				<tr>
+					<td>Установка/замена батарейки на материнской плате</td>
+					<td>200</td>
+				</tr>
+
+				<tr>
+					<th colspan="3" class="text-center" scope="col">Ноутбук (моноблок)</th>
+				</tr>
+
+				<tr>
+					<td>Техническое обслуживание (чистка, смазка вентиляторов, замена термопасты)</td>
+					<td>1200</td>
+				</tr>
+				<tr>
+					<td>Замена комплектующих с полной разборкой ноутбука</td>
+					<td>1200</td>
+				</tr>
+				<tr>
+					<td>Замена матрицы</td>
+					<td>от 1000</td>
+				</tr>
+				<tr>
+					<td>Замена со снятием крышки отсека, замена клавиатуры</td>
+					<td>400</td>
+				</tr>
+				<tr>
+					<td>Замена аккумулятора</td>
+					<td>100</td>
+				</tr>
+				<tr>
+					<td>Замена материнской платы</td>
+					<td>1200</td>
+				</tr>
+				<tr>
+					<td>Ремонт разъема питания, LAN и др.</td>
+					<td>от 1500</td>
+				</tr>
+				<tr>
+					<td>Установка/замена батарейки на материнской плате</td>
+					<td>1200</td>
+				</tr>
+
+				<tr>
+					<th colspan="3" class="text-center" scope="col">Программное обеспечение</th>
+				</tr>
+
+				<tr>
+					<td>Установка ОС Windows, Linux (+драйверы, первичная настройка)</td>
+					<td>900</td>
+				</tr>
+				<tr>
+					<td>Установка ОС Windows, Linux (без настройки)</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Установка Microsoft Office</td>
+					<td>300</td>
+				</tr>
+				<tr>
+					<td>Снятия пароля с операционной системы</td>
+					<td>400</td>
+				</tr>
+				<tr>
+					<td>Программная чистка</td>
+					<td>200</td>
+				</tr>
+				<tr>
+					<td>Установка, восстановление, обновление BIOS</td>
+					<td>1200</td>
+				</tr>
+
+				<tr>
+					<th colspan="3" class="text-center" scope="col">Работа с данныыми</th>
+				</tr>
+
+				<tr>
+					<td>Копирование информации с одного носителя на другой, за 1 Гб</td>
+					<td>30</td>
+				</tr>
+				<tr>
+					<td>Восстановление данных</td>
+					<td>от 400</td>
+				<tr>
+					<td>Восстановление логической структуры жесткого диска</td>
+					<td>500</td>
+				</tr>
+				<tr>
+					<td>Восстановление носителей информации</td>
+					<td>от 400</td>
+				</tr>
+
+				<tr>
+					<th colspan="3" class="text-center" scope="col">Настройка интернета</th>
+				</tr>
+
+				<tr>
+					<td>Настройка роутера</td>
+					<td>500</td>
+				</tr>
+				<tr>
+					<td>Обжим сетевого кабеля</td>
+					<td>50</td>
+				</tr>
+
+			</tbody>
+
+		</table>
+
+	</div>
+
+
+
 
 
 	<div class="call">
